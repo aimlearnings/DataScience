8c6def2ecfc77bb1eb717a51ed1d6d34a1d062b3
aimlearnings <info.aimlearnings@gmail.com>
2019-11-30 19:39:29 +0530

    Adding PPTs

--- OOPs/HelloWorld.py
+++
@@ -1,7 +0,0 @@
-def justPrint(text):
-    '''This function prints the text passed as argument to this function'''
-    print(
-        text)
-
-if __name__ == '__main__':
-    justPrint('Welcome')
---
+++ OOPs/SampleModule.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+anAttribute = 'Sample Attribute'
+
+
+# In[6]:
+
+
+def greeting(name):
+  print("Hello, " + name)
+
+
+# In[ ]:
+
+
+
+
