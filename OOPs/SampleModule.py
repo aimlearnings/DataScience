3787137a43d0f9dd32a6930f128f6ad5582d352a
aimlearnings <info.aimlearnings@gmail.com>
2020-02-18 12:20:27 +0530

    Adding PPTs

--- OOPs/SampleModule.py
+++ OOPs/SampleModule.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
+ 
 
 anAttribute = 'Sample Attribute'
 
-
-# In[6]:
-
+ 
 
 def greeting(name):
   print("Hello, " + name)
 
-
-# In[ ]:
-
-
-
+ 
 
